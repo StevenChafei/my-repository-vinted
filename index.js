5bd19a5e0a5ec405184223d42dc8833fc349da9b
Steven Chafei <stevenchafei@MacBook-Air-de-Steven.local>
2023-01-20 17:21:33 +0100

    last version

--- index.js
+++ index.js
@@ -1,23 +1,27 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
-const fileUpload = require("express-fileupload");
+// const fileUpload = require("express-fileupload");
 
-const isAuthenticated = require("./middlewares/isAuthenticated");
+// const isAuthenticated = require("./middlewares/isAuthenticated");
 
 const cloudinary = require("cloudinary").v2;
 
+require("dotenv").config();
+
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 mongoose.set("strictQuery", false);
-mongoose.connect("mongodb://localhost:27017/andromeda-vinted");
+mongoose.connect(process.env.MONGODB_URI);
 
 // Données à remplacer avec les vôtres :
 cloudinary.config({
-  cloud_name: "dsoydeobc",
-  api_key: "336911827334496",
-  api_secret: "P6Ra-NnE7h8o4xZXhPozcqQjY2I",
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
   secure: true,
 });
 //
@@ -32,6 +36,6 @@ app.all("*", (req, res) => {
   res.status(404).json({ message: "This routes doesn't exist" });
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
   console.log("Server started 🧦");
 });
